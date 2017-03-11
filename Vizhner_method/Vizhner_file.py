2e96eca7263ab531b5b1314476f274e729896a54
Bekzat Shayakhmetov <bekzat.95@mail.ru>
2017-03-11 11:25:47 +0700

    file encode/decode added

--- Vizhner_method/Vizhner_file.py
+++ Vizhner_method/Vizhner_file.py
@@ -1,29 +1,32 @@
-from Caesar_method.Caesar import Caesar
-caesar = Caesar()
+from Vizhner_method.Vizhner import Vizhner
 
 
-def file_reader_decoder(key, file_path):
-    count = 0
+def file_reader_decoder(key, file_path, lang):
+    words_to_encode = ''
     print("Результат: ")
     file = open(file_path, 'r')
     for line in file:
-        count += 1
-        print("Строка №" + str(count) + ": " + caesar.caesar_decode(key, line))
+        words_to_encode += line
+    vizhner = Vizhner(key, words_to_encode, lang)
+    vizhner.do_decode()
 
 
-def file_reader_encoder(key, file_path):
-    count = 0
+def file_reader_encoder(key, file_path, lang):
     print("Результат: ")
     file = open(file_path, 'r')
+    words_to_encode = ''
     for line in file:
-        count += 1
-        print("Строка №" + str(count) + ": " + caesar.caesar_encode(key, line))
+        words_to_encode += line
+    vizhner = Vizhner(key, words_to_encode, lang)
+    vizhner.do_encode()
 
 
-def main():
+if __name__ == "__main__":
     while True:
         print("\nХотите зашифровать(1) или расшифровать(2) файл?")
         answer = int(input())
+        print("Языки английский(1) || русский(2)")
+        language = int(input())
 
         if answer == 1 or answer == 2:
             print("Введи ключ: ")
@@ -31,8 +34,8 @@ def main():
             print("Введите путь к файлу :")
             path = input().strip()  # Удаление пробелов спереди и сзади
             if answer == 1:
-                file_reader_encoder(key_to_encode_decode, path)
+                file_reader_encoder(key_to_encode_decode, path, language)
             elif answer == 2:
-                file_reader_decoder(key_to_encode_decode, path)
+                file_reader_decoder(key_to_encode_decode, path, language)
         else:
             print("Ошибка!")
